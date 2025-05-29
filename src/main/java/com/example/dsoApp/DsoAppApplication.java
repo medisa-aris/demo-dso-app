105dc84d11e690a5238d2d0369bb25a649e040a6
Medisa Aris Ginanjar <medisa.aris@eris.co.id>
2025-05-29 22:54:43 +0700

    initial springboot commit

---
+++ src/main/java/com/example/dsoApp/DsoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.dsoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsoAppApplication.class, args);
+	}
+
+}
